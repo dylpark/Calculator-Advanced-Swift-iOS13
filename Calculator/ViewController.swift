e5c486dfca47a2836736473ab29882d9b5cffd6d
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-05-10 15:52:24 +1000

    AC, 0, %, Functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,6 +18,20 @@ class ViewController: UIViewController {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number * 0.01)
+            }
+        }
     }
 
     
