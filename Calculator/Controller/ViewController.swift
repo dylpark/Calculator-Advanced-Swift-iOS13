764b67eb6c66c9d76feee7ef2d8bdb9dd98b331b
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-05-10 16:42:10 +1000

    Starting MVC

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -14,27 +14,33 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double")
-        }
-        
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
             } else if calcMethod == "%" {
-                displayLabel.text = String(number * 0.01)
-            }
+                displayValue *= 0.01            }
         }
     }
 
-    
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
@@ -47,11 +53,7 @@ class ViewController: UIViewController {
                 
                 if numValue == "." {
                     
-                    guard let currentDisplayValue = Double (displayLabel.text!) else {
-                        fatalError("Cannot convert displayLabel.text to a Double")
-                    }
-                    
-                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    let isInt = floor(displayValue) == displayValue
                     
                     if !isInt {
                         return
@@ -63,5 +65,6 @@ class ViewController: UIViewController {
         }
     }
 
+    
 }
 
---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Dylan Park on 10/5/21.
+//  Copyright © 2021 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorLogic {
+    
+}
