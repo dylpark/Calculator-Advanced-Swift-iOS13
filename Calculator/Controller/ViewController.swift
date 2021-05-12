c98f0080a46f372d49cf2150619ce89744f3dcb3
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-05-12 14:12:57 +1000

    Adding tuples, switch statements, guard let vs if let

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     private var displayValue: Double {
         get {
             guard let number = Double(displayLabel.text!) else {
-                fatalError("Cannot convert display label text to a Double")
+                fatalError("Cannot convert display label text to a Double.")
             }
             return number
         }
@@ -29,25 +29,26 @@ class ViewController: UIViewController {
     private var calculator = CalculatorLogic()
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-
+        
         //What should happen when a non-number button is pressed
+        
         isFinishedTypingNumber = true
         
         calculator.setNumber(displayValue)
         
         if let calcMethod = sender.currentTitle {
-            
-            guard let result = calculator.calculate(symbol: calcMethod) else {
-                fatalError("The result of the calculation is nil.")
+ 
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
             }
-            displayValue = result
         }
-        
     }
 
+    
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        
         if let numValue = sender.currentTitle {
             
             if isFinishedTypingNumber {
@@ -63,12 +64,9 @@ class ViewController: UIViewController {
                         return
                     }
                 }
-                
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
     }
 
-    
 }
-
